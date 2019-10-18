4f8d677c2cb878766c0adbb4e2a7c552e7787aae
conorw8 <conorw8@gmail.com>
2019-10-18 13:41:48 -0500

    fixed the noise simulation

--- src/ugv_simulator.py
+++ src/ugv_simulator.py
@@ -93,8 +93,8 @@ while not rospy.is_shutdown():
     t.header.stamp = rospy.Time.now()
     t.header.frame_id = "odom";#"base_link"
     t.child_frame_id = "lilbot_SIMULATION/Base Frame"
-    t.transform.translation.x = test_x*noise_level
-    t.transform.translation.y = test_y*noise_level
+    t.transform.translation.x = test_x+np.random.rand()*noise_level
+    t.transform.translation.y = test_y+np.random.rand()*noise_level
     t.transform.translation.z = 0.0
     q = tf_conversions.transformations.quaternion_from_euler(0, 0, test_theta)
     t.transform.rotation.x = q[0]
