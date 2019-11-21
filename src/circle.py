9ac7383640cd732a6b0cac8f84e5196265fd1672
Conor Wallace <conorw8@gmail.com>
2019-11-21 12:52:36 -0600

    stuff

---
+++ src/circle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import math
+import tf2_ros
+import tf_conversions
+import geometry_msgs.msg
+import turtlesim.srv
+import numpy as np
+from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int16
+from network_faults.msg import Velocity
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import euler_from_quaternion
+import matplotlib.pyplot as plt
+global xv, wv, last_x, last_last_x, xv1, xv2, g_enabled, publishers, DEBUG_ENABLED, last_time, g_testing_enabled
+
+if __name__ == '__main__':
+
+    rospy.init_node('lilbot_controller_tf2_listener')
+    lilbotPub = rospy.Publisher("/lilbot_6ADE87/cmd", Int16MultiArray, queue_size=1, tcp_nodelay=True)
+    msg = Int16MultiArray()
+    rate = rospy.Rate(10.0)
+    xv1 = xv2 = xv = wv = 0
+    while not rospy.is_shutdown():
+        xv1 = 175
+        xv2 = 175
+        wv = 250
+
+        msg.data = [int(-xv1),int(-xv2),0,int(-wv)]
+        lilbotPub.publish(msg)
+
+        rate.sleep()
--- src/get_data.py
+++ src/get_data.py
@@ -25,58 +25,42 @@ z = 0
 linear_vel = 0
 angular_vel = 0
 path_data = []
+msg_data = []
 stop = 0
 
-def gotdata(txrx, vel, pose):
-    global txrx_pl,txrx_td, roll, pitch, yaw, x, y, z, linear_vel, angular_vel, path_data, stop
+def gotdata(txrx, imu, pose):
+    global stop, msg_data
 
     if not stop:
-        if vel != None:
-            txrx_pl = txrx.packet_loss
-            txrx_td = txrx.time_delay
-            linear_vel = vel.lin_vel
-            angular_vel = vel.ang_vel
-            x = pose.pose.position.x
-            y = pose.pose.position.y
-            z = pose.pose.position.z
-            quaternion = (
-                pose.pose.orientation.x,
-                pose.pose.orientation.y,
-                pose.pose.orientation.z,
-                pose.pose.orientation.w)
-            euler = euler_from_quaternion(quaternion)
-            roll = euler[0]
-            pitch = euler[1]
-            yaw = euler[2]
+        quaternion = (
+            pose.pose.orientation.x,
+            pose.pose.orientation.y,
+            pose.pose.orientation.z,
+            pose.pose.orientation.w)
+        euler = euler_from_quaternion(quaternion)
+        roll = euler[0]
+        pitch = euler[1]
+        yaw = euler[2]
 
-            path_data.append([txrx_pl, txrx_td, linear_vel, angular_vel, x, y, z, roll, pitch, yaw])
+        msg_data.append([txrx.time_delay, pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, roll, pitch, yaw, imu.roll, imu.pitch, imu.yaw, imu.accel_y, imu.accel_z, imu.lin_x, imu.lin_y, imu.lin_z])
 
-            print("Packet Loss: %s, Time Delay: %s" % (txrx_pl, txrx_td))
-            print("Linear Velocity: %s, Angular Velocity: %s" % (linear_vel, angular_vel))
-            print("Roll: %s, Pitch: %s, Yaw: %s" % (roll, pitch, yaw))
-            print("X: %s, Y: %s, Z: %s" % (x, y, z))
-            print("length of data: %s" % len(path_data))
-
-            if len(path_data) == 200:
-                stop = 1
-        else:
-            print("no messages received yet")
+        if len(msg_data) == 200:
+            stop = 1
 
 rospy.init_node('GetData', anonymous=True)
 network_sub = message_filters.Subscriber("network_stats", Network)
-vel_sub = message_filters.Subscriber("lilbot_vel", Velocity)
-pose_sub = message_filters.Subscriber("lilbot_3BA615/pose_152", PoseStamped)
+pose_sub = message_filters.Subscriber("lilbot_6ADE87/pose_152", PoseStamped)
+imu_sub = message_filters.Subscriber("/imu", IMU)
 
-ats = message_filters.ApproximateTimeSynchronizer([network_sub, vel_sub, pose_sub],10,0.2)
+ats = message_filters.ApproximateTimeSynchronizer([network_sub, imu_sub, pose_sub],10,0.2)
 ats.registerCallback(gotdata)
 
 rate = rospy.Rate(10.0)
 while not rospy.is_shutdown():
     if stop:
         print("converting data to csv")
-        path_data = np.asarray(path_data)
-        path_data = np.reshape(path_data, [200, 10])
-        print(path_data)
+        msg_data = np.asarray(msg_data)
+        msg_data = np.reshape(msg_data, [200, 15])
 
         f=open("../data/path_data.csv",'a')
         np.savetxt(f, path_data, delimiter=",")
